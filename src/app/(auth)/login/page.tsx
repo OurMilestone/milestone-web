41af8627fc92c1d449dc47f05963f95263ec1d17
chuba <chinemelumchubanwene57@gmail.com>
2025-05-19 14:09:46 +0100

    feat: create login and forgot password page UI

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,7 +1,14 @@
+import LoginForm from "@/components/auth/login/login-form";
+import type { Metadata } from "next";
 import React from "react";
 
+export const metadata: Metadata = {
+	title: "Login | Milestone",
+	description: "Login to your account",
+};
+
 const Login = () => {
-	return <div>Login</div>;
+	return <LoginForm />;
 };
 
 export default Login;
