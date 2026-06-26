bc5f36bcd2f5cfc5d0deefd458b7e3e1f9d1ee2d
Israel Abiona <adeyanjuabiona69@gmail.com>
2026-06-26 13:31:25 +0100

    Revert "feat: handle 401"

--- src/utils/auth.ts
+++
@@ -1,9 +0,0 @@
-import { AppRoutePaths } from "@/config/routes-config";
-import { signOut } from "next-auth/react";
-
-export function logoutUser() {
-	localStorage.removeItem("authToken");
-	localStorage.removeItem("refreshToken");
-	localStorage.removeItem("user");
-	return signOut({ redirect: true, callbackUrl: AppRoutePaths.SignIn });
-}
